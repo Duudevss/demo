ad62dde1dad405f54016e20e8f8555bedf382792
Patinz <duudevss1223@gmail.com>
2025-03-17 17:35:06 +0700

    Learn operators

---
+++ operators.py
@@ -0,0 +1,47 @@
+x = 5
+y = 2 
+
+a1 = x + y   # 7
+a2 = x - y   # 3
+a3 = x * y   # 10
+a4 = x / y   # 2.5
+a5 = x % y   # 1
+a6 = x ** y  # 25
+a7 = x // y  # 2
+
+a8 = (x + 1) * (y - 1)
+
+print(a8)
+
+# short cut operator
+
+a = 2
+
+a += 3
+
+print(x)
+
+b = 8
+c = 4
+
+a9 = x > y    # true
+a10 = x >= y  # true
+
+a11 = x < y   # false
+a12 = x <= y  # false
+
+a13 = x == y  # false 
+a14 = x != y  # true
+
+print(a13)
+
+
+# Logic 
+p = True
+q = False
+
+t1 = p and q   # false
+t2 = p or q    # True
+t3 = not p     # false
+
+print(t2)
