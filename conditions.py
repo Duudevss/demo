7103ce89c21395dacbef4e4cf9829fb595517d1c
Patinz <duudevss1223@gmail.com>
2025-03-17 17:51:23 +0700

    Learn conddition

---
+++ conditions.py
@@ -0,0 +1,13 @@
+score = 45
+
+if score >= 80:
+    print('Grade A')
+    print('Good Job!!')
+elif score >= 70:
+    print('Grade B')
+elif score >= 60:
+    print('Grede C')
+elif score >= 50:
+    print('Grade D')
+else :
+    print('Grade F')
